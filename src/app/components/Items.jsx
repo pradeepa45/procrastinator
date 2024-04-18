f2ed85a78feee7873504ee82d7b0e7900a7edffc
pradeepa45 <pradeepa.seelam@gmail.com>
2024-04-19 00:33:32 +0530

    UI: view tasks list

--- src/app/components/Items.jsx
+++ src/app/components/Items.jsx
@@ -1,7 +1,40 @@
+'use client'
+
+import React from "react"
+import { Button, Confirm, Icon } from "semantic-ui-react"
+
+const dummyData = [
+  {name: `Organise flowers for bestie's surprise birthday party`, status: 'done', priority: 'normal'},
+  {name: 'Send out invites', status: '', priority: 'low'},
+  {name: 'Renew subscription', status: '', priority: 'critical'},
+  {name: 'Prepare for interviews', status: 'done', priority: 'high'},
+  {name: 'Reach out to client regarding an estimate', status: '', priority: 'normal'},
+  {name: 'Check out the new store in the neighbourhood', status: '', priority: 'low'},
+  {name: 'Annual health check up appointment', status: 'done', priority: 'critical'},
+  {name: 'List out groceries for my next shopping', status: 'done', priority: 'normal'},
+]
+
 export default function Tasks() {
+  const [priority, setPriorityFilter] = React.useState('')
+  const [status, setStatusFilter] = React.useState('')
+  
   return (
-    <div>
-      Tasks
+    <div className="basis-1/3 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 p-5 relative flex">
+      <div className="absolute top-4 flex items-start gap-4 right-4">
+        <Button icon labelPosition="right" className="!bg-yellow-400">Priority <Icon name="filter" /></Button>
+        <Button icon labelPosition="right" className="!bg-yellow-400">Status <Icon name="filter" /></Button>
+      </div>
+      <div className="flex flex-col gap-2 justify-center">
+        {dummyData.map((item, index)=>(
+          <div className="flex gap-1" key={index}>
+            <Button className="!border !border-white" circular></Button>
+            <p className={item.status === 'done' ? 'line-through grow mt-2 font-semibold': "grow mt-2"}>{item.name}</p>
+            <Button className="!border !border-white !bg-transparent" icon circular>
+              <Icon name="trash" color="yellow" />
+            </Button>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,43 @@
+'use client'
+
+import React from "react"
+import { Button, Sidebar, SidebarPushable, SidebarPusher, TransitionGroup } from "semantic-ui-react"
+
 import AddTask from "./components/AddItem"
 import Tasks from "./components/Items"
 
 export default function Home() {
+  const [showViewTasks, setViewTasks] = React.useState(false)
+
+  const handleClick = () => {
+    setViewTasks(!showViewTasks)
+  }
+  
   return (
-    <main className="">
+    <main>
     <div className="flex justify-between">
-      <div className="p-4">
-        <nav>
-          <p class="bg-clip-text text-transparent bg-gradient-to-r to-white from-yellow-400 text-5xl font-black">
+      <div className="grow">
+        <nav className="p-5">
+          <p className="bg-clip-text text-transparent bg-gradient-to-r to-white from-yellow-400 text-5xl font-black w-fit">
             Procrastinator
           </p>
         </nav>
       </div>
-      <div className="px-5 min-h-screen flex flex-col gap-10 justify-center  basis-1/3 overflow-y-scroll  bg-white text-yellow-400">
-        <AddTask />
+      <div className="h-screen relative flex flex-col gap-10 justify-center basis-1/3 overflow-y-scroll bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10">
+        <div className="absolute top-5 right-5">
+          <Button onClick={handleClick} className="!bg-yellow-400">{showViewTasks ? "Hide" : "Show"} all tasks</Button>
+        </div>
+        <div className="px-5">
+        <AddTask handleClick={handleClick} />
+        </div>
       </div>
+      {showViewTasks && (
+      <TransitionGroup animation="slide left" duration={1000}>
+          <Tasks />
+      </TransitionGroup>
+    )}
+
     </div>
-    <Tasks />
     </main>
   );
 }
