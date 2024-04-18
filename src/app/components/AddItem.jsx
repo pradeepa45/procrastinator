4057d5036459121506f2c7649baf863f14026c6e
pradeepa45 <pradeepa.seelam@gmail.com>
2024-04-18 18:36:32 +0530

    feat: new UI basic layout

---
+++ src/app/components/AddItem.jsx
@@ -0,0 +1,57 @@
+'use client'
+
+import React from "react"
+import { Button, Dropdown, Form, Input } from "semantic-ui-react"
+
+
+const priorities = [
+  {key: 'critical', text: 'Critical', value:'critical'},
+  {key: 'high', text: 'High', value:'high'},
+  {key: 'normal', text: 'Normal', value:'normal'},
+  {key: 'low', text: 'Low', value:'low'},
+]
+
+const initialState = {
+  name: '',
+  priority: 'Normal',
+}
+
+export default function AddTask() {
+  const [task, setTask] = React.useState(initialState)
+  const [submitting, setSubmitting] = React.useState(false)
+  const [submitted, setSubmitted] = React.useState(false)
+
+  const onChange = (e,data) => {
+    const {name, value} = data
+    setTask({
+      ...task,
+      [name]: value
+    })
+  }
+
+  const addTask = () => {
+    setSubmitting(true)
+    // submit task
+    setTask(initialState)
+    setSubmitting(false)
+  }
+
+  return (
+    <div className=" flex flex-col justify-center py-10">
+      <p className="text-2xl mb-5 text-center">Add task</p>
+      <Form onSubmit={addTask} className="flex flex-col gap-4 fluid">
+        <Input placeholder="New task" onChange={onChange} name="name" value={task.name} loading={submitting} required/>
+        <Dropdown
+          placeholder='Priority (normal)'
+          fluid
+          search
+          selection
+          defaultValue="normal"
+          options={priorities}
+          disabled={submitting}
+        />
+        <Button className="!bg-yellow-400 !text-white px-1.5 py-5" type="submit">Add</Button>
+      </Form>
+    </div>
+  )
+}
